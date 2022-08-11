60053c3e38978106cdd750ee72531c592d17d514
Inderpreet02 <inder192002@gmail.com>
2022-08-11 14:59:27 +0530

    fetched data and added navbar

---
+++ index.js
@@ -0,0 +1,16 @@
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // document.getElementById("demo").src = JSON.parse(this.responseccText);
+            // localStorage.setItem('data', JSON.parse(this.responseText));
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09", true);
+    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhttp.send();
+}
+
+
+loadDoc();
