0b43d05985c3b462fdb433ceedbb3fd722f8b68b
Inderpreet02 <inder192002@gmail.com>
2022-08-11 16:48:43 +0530

    made albums array and main section

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ function loadDoc() {
         if (this.readyState == 4 && this.status == 200) {
             // document.getElementById("demo").src = JSON.parse(this.responseccText);
             // localStorage.setItem('data', JSON.parse(this.responseText));
-            console.log(this.responseText);
+            // console.log(this.responseText);
+            const users = [];
+            const data = JSON.parse(this.responseText);
+
+            data.forEach(element => {
+                if(!users.includes(element.albumId)){
+                    users.push(element.albumId);
+                }
+                // console.log(element);
+            });
+
+            console.log(users);
         }
     };
     xhttp.open("GET", "https://jsonplaceholder.typicode.com/photos?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09", true);
